d87313b9dd44df24d6d2eb1becc777fd29d0ccbd
sahilpabale <dev.sahilpabale@gmail.com>
2023-09-27 19:47:27 +0530

    redis husky changes

---
+++ packages/redis/index.ts
@@ -0,0 +1,6 @@
+import type { RedisOptions } from "ioredis";
+import { Redis } from "ioredis";
+
+export const createRedisInstance = (options: RedisOptions): Redis => {
+  return new Redis(options);
+};
