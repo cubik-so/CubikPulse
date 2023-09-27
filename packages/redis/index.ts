b64a5fbad7f2a2b2b687de9c179494c3bfbc03e8
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-09-27 10:09:30 +0530

    feat: added husky

--- packages/redis/index.ts
+++ packages/redis/index.ts
@@ -1,4 +1,4 @@
-import Redis, { RedisOptions } from 'ioredis';
+import Redis, { RedisOptions } from "ioredis";
 
 export const createRedisInstance = (options: RedisOptions) => {
   return new Redis(options);
