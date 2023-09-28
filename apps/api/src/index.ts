6dad331f38b8b378cf5a0b990ddcdebd2ecefe57
sahilpabale <dev.sahilpabale@gmail.com>
2023-09-28 21:53:35 +0530

    feat: common types package added

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,11 @@
 import cors from "cors";
 import express, { Express, Request, Response } from "express";
-import { createRedisInstance } from "@pulse/redis";
-const app: Express = express();
 import dotenv from "dotenv";
 import { EventEmitter } from "stream";
+import { createRedisInstance } from "@pulse/redis";
+import type { WebhookInput } from "@pulse/types";
+
+const app: Express = express();
 EventEmitter.defaultMaxListeners = 100;
 
 dotenv.config();
@@ -31,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/hook", async (req: Request, res: Response) => {
   try {
     const ixName = req.body[0].meta.logMessages[1].split("Instruction:")[1]; // TODO: can not be determined for all transactions, need a new way to get the name
-    const object = {
+    const object: WebhookInput = {
       timestamp: req.body[0].blockTime,
       sig: req.body[0].transaction.signatures[0],
       name: ixName ? ixName.trim() : "",
---
+++ packages/types/src/indexer.ts
@@ -0,0 +1,6 @@
+export type WebhookInput = {
+  timestamp: number;
+  sig: string;
+  name: string;
+  type: string;
+};
