c8fadf5781403f50756b0715915ac0a436616ff6
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-09-28 16:35:12 +0530

    fix: Updated round robin

--- apps/RPC/src/index.ts
+++ apps/RPC/src/index.ts
@@ -4,8 +4,12 @@ interface Env {
 	URL2: string;
 }
 
+let currentIndex: number = 0;
+
 export default {
 	async fetch(request: Request, env: Env) {
+		const RPC_URL = [env.URL1, env.URL2];
+
 		const supportedDomains = env.CORS_ALLOW_ORIGIN ? env.CORS_ALLOW_ORIGIN.split(',') : undefined;
 		const corsHeaders: Record<string, string> = {
 			'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, OPTIONS',
@@ -28,8 +32,10 @@ export default {
 		}
 
 		const payload = await request.text();
+		const currentRpcUrl = RPC_URL[currentIndex];
+		currentIndex = (currentIndex + 1) % RPC_URL.length;
 
-		const proxyRequest = new Request('', {
+		const proxyRequest = new Request(currentRpcUrl, {
 			method: request.method,
 			body: payload || null,
 			headers: {
